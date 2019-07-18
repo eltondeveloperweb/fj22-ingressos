a9c044013fe3c2f631b9d9961e0d3ec999965f51
Elton Silva <eltonajs@gmail.com>
2019-07-17 22:38:03 -0300

    Inicio implementação validação Sessão.

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -12,7 +12,7 @@ import javax.persistence.ManyToOne;
 public class Sessao {
 	
 	@Id	
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	
 	private LocalTime horario;
